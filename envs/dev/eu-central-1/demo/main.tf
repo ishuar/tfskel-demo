a73e09941660cd290b157c437ae8d75a3efba8f4
ishuar <ishansharma887@gmail.com>
2026-03-02 12:16:39 +0100

    fix tf lint and removed tag
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- envs/dev/eu-central-1/demo/main.tf
+++ envs/dev/eu-central-1/demo/main.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "tfskel_demo" {
   bucket = "my-demo-dev-euc1-bucket"
 
   tags = {
-    Name      = "my-demo-dev-euc1-bucket"
-    "changed" = "workflow"
+    Name = "my-demo-dev-euc1-bucket"
   }
-}
-
-locals {
-  fail_tflint = true
 }
