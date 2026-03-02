4b76cf66f5e392e3aab8c2f276d9c66f11b11afc
ishuar <ishansharma887@gmail.com>
2026-03-02 11:56:44 +0100

    test latest tf workflows with tf changes
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- envs/dev/eu-central-1/demo/main.tf
+++ envs/dev/eu-central-1/demo/main.tf
@@ -2,7 +2,8 @@ resource "aws_s3_bucket" "tfskel_demo" {
   bucket = "my-demo-dev-euc1-bucket"
 
   tags = {
-    Name = "my-demo-dev-euc1-bucket"
+    Name      = "my-demo-dev-euc1-bucket"
+    "changed" = "workflow"
   }
 }
 
