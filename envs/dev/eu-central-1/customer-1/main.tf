68823f52c5c3fd7862c6cf36f30ae14d5d4594ca
ishuar <ishansharma887@gmail.com>
2026-05-06 13:32:56 +0200

    added variable to verify extra secrets
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- envs/dev/eu-central-1/customer-1/main.tf
+++ envs/dev/eu-central-1/customer-1/main.tf
@@ -1,8 +1,14 @@
 ## tfskel-source: {"template":"tf/main.tf.tmpl","hash":"98ee38e0570a47b1"}
+variable "TAG_FROM_GITHUB_SECRET" {
+  type      = string
+  sensitive = true
+}
+
 resource "aws_s3_bucket" "tfskel_demo" {
   bucket = "my-customer-1-dev-euc1-bucket"
 
   tags = {
-    Name = "my-customer-1-dev-euc1-bucket"
+    Name                   = "my-customer-1-dev-euc1-bucket"
+    tag_from_github_secret = var.TAG_FROM_GITHUB_SECRET
   }
 }
